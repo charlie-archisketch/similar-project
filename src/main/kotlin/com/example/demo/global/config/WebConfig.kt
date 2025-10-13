02da922ecd8bcddbbfc6c256fd4aca6a4a180b09
Charlie <charlie@archisketch.com>
2025-10-13 13:25:03 +0900

    refactor: import

--- src/main/kotlin/com/example/demo/global/config/WebConfig.kt
+++ src/main/kotlin/com/example/demo/global/config/WebConfig.kt
@@ -1,9 +1,8 @@
 package com.example.demo.global.config;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig: WebMvcConfigurer {
